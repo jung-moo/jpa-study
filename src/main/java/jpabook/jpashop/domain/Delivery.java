97dc45baa56961ac12bb94c82cf9388968c67d57
jmh3084 <jm3084@naver.com>
2026-08-29 23:45:12 +0900

    @XToOne 관계들을 모두 지연로딩으로 설정

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -13,7 +13,8 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+    //“FK가 없는 반대쪽에서는 연관 객체 존재 여부를 알기 어려워 LAZY가 제대로 적용되지 않을 수 있다” 라는 내용이 있지만 우선은 수업을 따라감
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
     @Embedded
