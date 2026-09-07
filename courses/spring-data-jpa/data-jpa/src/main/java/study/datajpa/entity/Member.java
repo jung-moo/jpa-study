3f2d15a061958957e349b4e7a06b7883cb73a6a7
jmh3084 <jm3084@naver.com>
2026-09-07 20:25:22 +0900

    Member 엔티티 수정

--- courses/spring-data-jpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ courses/spring-data-jpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -10,7 +10,16 @@ import lombok.Setter;
 @Entity
 @Getter @Setter
 public class Member {
+
     @Id @GeneratedValue
     private Long id;
     private String username;
+
+    protected Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+
 }
