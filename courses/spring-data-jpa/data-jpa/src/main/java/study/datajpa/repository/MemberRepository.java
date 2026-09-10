91e169918cbf137d76fe3f96f548a28cca07ee82
jmh3084 <jm3084@naver.com>
2026-09-11 08:23:58 +0900

    스프링 데이터 JPA 페이징과 정렬 학습

--- courses/spring-data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ courses/spring-data-jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,5 +1,7 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -26,5 +28,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username in :names")
     List<Member> findByNames(@Param("names") Collection<String> names);
+
+    Page<Member> findByAge(int age, Pageable pageable);
+
+    @Query(value = "select m from Member m",
+            countQuery = "select count(m.username) from Member m")
+    Page<Member> findMemberAllCountBy(Pageable pageable);
 
 }
